1b944ae612cba4b23b18e3539d5f9980264d4ebe
4nanda <dika23nanda@gmail.com>
2023-08-13 13:03:10 +0700

    first commit

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,116 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+    <div class="app-brand demo">
+        <a href="/" class="app-brand-link">
+            <span class="app-brand-logo demo">
+                <img src="../assets/img/illustrations/jarvis-logo.png" class="w-25">
+            </span>
+        </a>
+        <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+            <i class="bx bx-chevron-left bx-sm align-middle"></i>
+        </a>
+    </div>
+
+    <div class="menu-inner-shadow"></div>
+
+    <ul class="menu-inner py-1">
+        <!-- Dashboard -->
+        <li class="menu-item">
+            <a href="/" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">Dashboard</div>
+            </a>
+        </li>
+        <!-- Tables -->
+        <li class="menu-item">
+            <a href="/table" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-table"></i>
+                <div data-i18n="Tables">Tables</div>
+            </a>
+        </li>
+        <!-- Account Settings -->
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-dock-top"></i>
+                <div data-i18n="Account Settings">Account Settings</div>
+            </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a href="pages-account-settings-account.html" class="menu-link">
+                        <div data-i18n="Account">Account</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="pages-account-settings-notifications.html" class="menu-link">
+                        <div data-i18n="Notifications">Notifications</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="pages-account-settings-connections.html" class="menu-link">
+                        <div data-i18n="Connections">Connections</div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <!-- Authentications -->
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-lock-open-alt"></i>
+                <div data-i18n="Authentications">Authentications</div>
+            </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a href="auth-login-basic.html" class="menu-link" target="_blank">
+                        <div data-i18n="Basic">Login</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="auth-register-basic.html" class="menu-link" target="_blank">
+                        <div data-i18n="Basic">Register</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="auth-forgot-password-basic.html" class="menu-link" target="_blank">
+                        <div data-i18n="Basic">Forgot Password</div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <!-- Forms -->
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-detail"></i>
+                <div data-i18n="Form Elements">Form Elements</div>
+            </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a href="forms-basic-inputs.html" class="menu-link">
+                        <div data-i18n="Basic Inputs">Basic Inputs</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="forms-input-groups.html" class="menu-link">
+                        <div data-i18n="Input groups">Input groups</div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-detail"></i>
+                <div data-i18n="Form Layouts">Form Layouts</div>
+            </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a href="form-layouts-vertical.html" class="menu-link">
+                        <div data-i18n="Vertical Form">Vertical Form</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="form-layouts-horizontal.html" class="menu-link">
+                        <div data-i18n="Horizontal Form">Horizontal Form</div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
